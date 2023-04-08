f24e9d29c5abdbd35532e9cd51e18ca06b3c32aa
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-04-08 05:30:34 -0700

    updating the parallax section in the body

---
+++ script.js
@@ -0,0 +1,8 @@
+// let nav = document.querySelector('.header');
+//        window.onscroll = function (){
+//         if(document.documentElement.scrollTop > 50){
+//         nav.classList.add('header-scrolled');
+//     }else{
+//         nav.classList.remove('header-scrolled');
+//         }
+//        }
