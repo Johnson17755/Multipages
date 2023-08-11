7d240cf6aee7aa5a88801ed52a06f8397dd2f752
Johnson17755 <112248001+Johnson17755@users.noreply.github.com>
2023-08-11 04:51:28 -0700

    updating

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// let nav = document.querySelector('.header');
-//        window.onscroll = function (){
-//         if(document.documentElement.scrollTop > 50){
-//         nav.classList.add('header-scrolled');
-//     }else{
-//         nav.classList.remove('header-scrolled');
-//         }
-//        }
+let nav = document.querySelector('.header');
+       window.onscroll = function (){
+        if(document.documentElement.scrollTop > 50){
+        nav.classList.add('header-scrolled');
+    }else{
+        nav.classList.remove('header-scrolled');
+        }
+       }
